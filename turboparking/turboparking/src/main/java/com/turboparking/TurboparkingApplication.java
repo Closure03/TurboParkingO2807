0729bce2e6d0465e7f09d5dceae2b67028bad75e
Closure03 <santicdlm27@gmail.com>
2022-08-26 22:03:12 -0500

    08/26/22

--- turboparking/turboparking/src/main/java/com/turboparking/TurboparkingApplication.java
+++ turboparking/turboparking/src/main/java/com/turboparking/TurboparkingApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.turboparking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class TurboparkingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(TurboparkingApplication.class, args);
 	}
 
 }
--- turboparking/turboparking/src/test/java/com/turboparking/TurboparkingApplicationTests.java
+++ turboparking/turboparking/src/test/java/com/turboparking/TurboparkingApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.turboparking;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class TurboparkingApplicationTests {
 
 	@Test
 	void contextLoads() {
