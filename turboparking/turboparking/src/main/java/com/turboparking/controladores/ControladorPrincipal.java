ae862e047df48acb582ef7b525c54e6ffa7dbfd9
AngeloGelvez <111929278+AngeloGelvez@users.noreply.github.com>
2022-08-30 18:47:23 -0500

    Avances Proyecto
    Se creo un archivo html y una clase en java.

---
+++ turboparking/turboparking/src/main/java/com/turboparking/controladores/ControladorPrincipal.java
@@ -0,0 +1,17 @@
+/*
+ * En estos paquetes y clases se controlan las páginas web
+ */
+package com.turboparking.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ControladorPrincipal {
+    
+    //Aca se envia el controlador de URL 
+    @GetMapping("/")
+    public String cargarPaginaPrincipal() {
+        return "index";
+    }
+}
